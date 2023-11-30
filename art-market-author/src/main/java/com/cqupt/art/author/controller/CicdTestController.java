b64a712ed8d20b16f2073c8a2f36b036ea3cb0dc
Your Name <hxd_a@qq.com>
2023-11-30 18:54:04 +0800

    cicd controller

---
+++ art-market-author/src/main/java/com/cqupt/art/author/controller/CicdTestController.java
@@ -0,0 +1,14 @@
+package com.cqupt.art.author.controller;
+
+import com.cqupt.art.utils.R;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CicdTestController {
+
+    @GetMapping("version")
+    public R version(){
+        return R.ok().put("version",1.0);
+    }
+}
